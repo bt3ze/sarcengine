12028936b82446c002d04457fe13b08c7780b639
Ben Terner <bt3ze@virginia.edu>
2014-05-03 15:08:20 -0400

    figuring out routes

--- engine.py
+++ engine.py
@@ -1,8 +1,14 @@
 import os
-from flask import Flask
+from flask import Flask, url_for, render_template, request,session
+import jinja2
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
+def engine():
+    return render_template('home.html',user='me')
+
+@app.route('/hello')
 def hello():
-    return render_template('home.html',{})
+    return "hello"
