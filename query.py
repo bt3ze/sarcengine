19fa3fc92a62ae52b5f743162ae5cce4de0a79d8
Ben Terner <bt3ze@virginia.edu>
2014-05-06 00:12:50 -0400

    try queries on the big dataset

--- query.py
+++ query.py
@@ -13,16 +13,25 @@ client = MongoClient()
 db = client.engine
 wordcollection = db.words
 relationcollection = db.relations
+lst = ["china","money","yen"]
 
-words = list(articlecollection.find({}))
+words = list(relationcollection.find({"word":lst}))
 
-#print words
+print words
 
+matches = []
+for w in words:
+    intersects = list(wordcollection.find({"words": {"$in": words}}))
+    print intersects
+
+
+'''
 for wlist in words:
 #    print wlist
     print wlist['words']
     for w in wlist['words']:
         relations = list(relationcollection.find({"word":w}))
-        print relations
-
+        for r in relations:
+            print r
+'''
 client.close()
